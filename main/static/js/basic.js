3c24a44840c8be66e128cf1e5885b98b279deaa4
gargshashank5 <garg.shashank.5@gmail.com>
2014-01-06 23:42:48 +0530

    adding nodes

--- main/static/js/basic.js
+++ main/static/js/basic.js
@@ -16,16 +16,17 @@ $("#drawMode").click(function(event) {
     var buttons = 
         "<button type='button' id='addNodesMode' onclick='initAddNodesMode();'> \
             Start Adding Nodes \
-        </button> \
-        <button type='button' id='addEdgesMode' onclick='initAddEdgesMode();'> \
+        </button>" 
+/*        <button type='button' id='addEdgesMode' onclick='initAddEdgesMode();'> \
             Start Adding Edges \
         </button> \
         <button type='button' id='postModel' onclick='postModel();'> \
             Post Model \
-        </button>"
+        </button>"*/
     $('#modeButtons').html (buttons);
 });
 
+/*
 $("#analysisMode").click(function(event) {
     //console.log("lund")
     currentState = 20;
@@ -60,30 +61,48 @@ function functions() {
 $("#cy").click(function(event) {
 console.log("Hello");
     }
-
-
 $(document).ready(function(event) {
     //console.log("bhag");
     //cy = this;
-});*/
+});
+*/
 
 $(loadCy=function() {
     options ={
         ready:function() {
-            cy=this;
+            cy = this;
         }
     };
     $("#cy").cytoscape(options);
 });
 
+//var node_id = "0";
+/*$("#harry").click(function(event) {
+    console.log("choot");});
+    
+$("#charry").click(function(event) {
+    console.log("bhoot");});
+*/
+/*User will click on #cy, a node will be added with a default name "defaultName". A box will pop up and will show "node i" default name. i is the current arr length+2 of node array. The name entered will be the div id of the node and the entry of array. */
+
 $("#cy").click(function(event) {
     if (currentState == 10) {
         pos = getMousePos(this, event);
-        nodeId = "defaultId";        
+        var nodeId = "default"
         cy.add([
-            {group: "nodes", data: {id: nodeId}, renderedPosition: pos},
+            {group: "nodes", data: {id: "default"}, renderedPosition: pos},
             ]);
-        $("#defaultId").click(function(event) {
-        console.log("chut");})
+        nodeId = prompt("node ka naam daal","node " + nodes.length);
+        console.log(nodeId);
+        nodes.push(nodeId);
+        //cy.nodes = "default").attr("id", nodeId);
+        //$("#default").attr("id", nodeId );
+        console.log(nodes);
+        //$("#def").attr("div", nodeId);
+        //nodeId = prompt("Please enter your name","harry");
+        //node_id = (parseInt(node_id)+1).toString();
+        //console.log("node_id " + node_id);
+        //console.log(nodeId);
     }
-});
+    }
+);
