76364c3a109753a20972050b4e84a0b2c0d470e3
e55k4y <samkent.1729@gmail.com>
2014-03-08 11:32:23 +0530

    Bug fixes for adding nodes and add edges feature developed.

--- main/static/js/basic.js
+++ main/static/js/basic.js
@@ -6,8 +6,8 @@ postModel = 13,
 analysisMode = 20,
 */
 var currentState = null;
-var nodesArray = new Array();
-var edgesArray = new Array();
+var edgeSource = null;
+var edgeTarget = null;
 
 // Initialize cytoscape.js
 $(loadCy=function() {
@@ -33,7 +33,7 @@ $("#drawMode").click(function(event) {
     currentState = 10;
     var buttons = 
         "<button type='button' id='addNodesMode' onclick='initAddNodesMode();'> Start Adding Nodes </button> \
-        <button type='button' id='addEdgesMode'> Start Adding Edges </button> \
+        <button type='button' id='addEdgesMode' onclick='initAddEdgesMode();'> Start Adding Edges </button> \
         <button type='button' id='postModel'> Post Model </button>";
     $('#modeButtons').html (buttons);
 });
@@ -59,17 +59,60 @@ $('#postModel').click(function(event) {
 $("#cy").click(function(event) {
     if (currentState == 11) {
         pos = getMousePos(this, event);
-	var nodeName = namePrompt(pos, 'node'+nodesArray.length);
-        var nodeId = 'node' + String(nodesArray.length);
-        cy.add([
-            {group: "nodes", data: {id: nodeId, name: "default"},
-            renderedPosition: pos},
-            ]);
-//TODO: reaplace this shitty promt with a kickass box for entering nodename.        
-        cy.elements("node#"+nodeId).data('name', nodeName);
-        nodesArray.push(nodeName);
-    }
+		var defName = 'node'+cy.nodes().length;
+		$("#nameForm").remove();
+		$("body").append("<form id='nameForm' class = 'well' style = 'height:90px ;width:200px ;position:relative ;'>");
+		$("#nameForm").append("<input id='textField' type='text' class='span3' placeholder=" + defName + ">");
+		$("#nameForm").append("<button id='textSubmit' type='submit' class='btn'>Submit</button>");
+		$("#nameForm").css({left:pos.x,top:pos.y});
+		$("#textSubmit").click(function(event){
+			if($("#textField").val()=="")
+				var nodeName = defName;
+			else
+				var nodeName = $("#textField").val();
+			$("#nameForm").remove();
+			cy.add([
+				{group: "nodes", data: {id: 'node'+cy.nodes().length, name: nodeName},renderedPosition: pos},
+				]); 
+			cy.nodes().unselect();
+			return false;
+		});
+	}
     else if (currentState == 12) {
-        //Code to add edges
+		if(cy.$("node:selected").data() ==null){
+			cy.nodes().unselect();
+		}
+		else{
+			if(edgeSource == null){
+				edgeSource = cy.$("node:selected").data("id");
+			}
+			else if (edgeSource !=null && edgeTarget == null){
+				edgeTarget = cy.$("node:selected").data("id");
+				if(edgeTarget != edgeSource && checkEdge(edgeSource,edgeTarget)){
+					cy.add([
+						{group:"edges", data : {id:'edge'+cy.edges().length, source:edgeSource, target:edgeTarget}},
+					]);
+					cy.style()
+					.selector('edge').css('target-arrow-shape','triangle')
+					.update()
+					}
+				edgeTarget = null;
+				edgeSource = null;
+			}
+		}
+		return false;
     }
 });
+
+function checkEdge(sourceId,targetId){
+	var targets = cy.elements('edge[source="'+sourceId+'"]').target();
+	if (targets==null)
+		var n = 0;
+	else
+		var n = targets.length;
+	for(var i=0;i<n;i++){
+		if (targetId==targets[i].data("id"))
+			return false;
+	}
+	return true;
+}
