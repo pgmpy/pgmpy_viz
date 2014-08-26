9f6c7ce3873416ee6d2dabe28b4bab88bb2ebef7
e55k4y <samkent.1729@gmail.com>
2014-08-27 00:16:47 +0530

    Nodes change color on selection. Automatic change of state between adding nodes and adding edges.

--- main/static/js/addNodes.js
+++ main/static/js/addNodes.js
@@ -2,10 +2,16 @@ function initAddNodesMode() {
     if (currentState == 11) {
         currentState = 10;
         $("#addNodesMode").text("Start Adding Nodes");
-		customAlert();
+        customAlert("Nodes are added.");
     }
     else if (currentState == 10) {
         currentState = 11;
+        $("#addNodesMode").text("Stop Adding Nodes");
+    }
+    else if (currentState == 12) {
+        currentState = 11;
+        $("#addEdgesMode").text("Start Adding Edges");
+        customAlert("Edges are added.");
         $("#addNodesMode").text("Stop Adding Nodes");
     }
     return;
